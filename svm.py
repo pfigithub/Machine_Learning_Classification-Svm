9a38f04eada623afc3114137b8dca0af44c6ad48
pfigithub <pampam7072@gmail.com>
2024-02-28 15:59:17 +0330

    selecting and pre-processing

--- svm.py
+++ svm.py
@@ -7,3 +7,14 @@ from sklearn.model_selection import train_test_split
 # reading data
 cell_df = pd.read_csv("cell_samples.csv")
 cell_df.head()
+
+# selecting and pre-processing
+cell_df = cell_df[pd.to_numeric(cell_df['BareNuc'], errors='coerce').notnull()]
+cell_df['BareNuc'] = cell_df['BareNuc'].astype('int')
+cell_df.dtypes
+
+feature_df = cell_df[['Clump', 'UnifSize', 'UnifShape', 'MargAdh', 'SingEpiSize', 'BareNuc', 'BlandChrom', 'NormNucl', 'Mit']]
+X = np.asarray(feature_df)
+
+cell_df['Class'] = cell_df['Class'].astype('int')
+y = np.asarray(cell_df['Class'])
